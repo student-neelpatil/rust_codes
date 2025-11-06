e9f54df224a2a1d16b00754db9b6be4dcec04318
student-neelpatil <patilneel288@gmail.com>
2025-11-06 13:47:46 +0530

    rust-if-else

---
+++ src/conditions.rs
@@ -0,0 +1,13 @@
+
+
+//If-else condition
+fn main() {
+    
+    let age=20;
+    
+    if age>18{
+        println!("he/she is an adult");
+    }else{
+        println!("he/she is a child");
+    }
+}
