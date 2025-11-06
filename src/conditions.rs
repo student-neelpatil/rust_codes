622778b523caba073ce529e876c5aea9176db08e
student-neelpatil <patilneel288@gmail.com>
2025-11-06 13:49:21 +0530

    rust-if-else

--- src/conditions.rs
+++ src/conditions.rs
@@ -1,13 +1,34 @@
 
 
-//If-else condition
+// //If-else condition
+// fn main() {
+    
+//     let age=20;
+    
+//     if age>18{
+//         println!("he/she is an adult");
+//     }else{
+//         println!("he/she is a child");
+//     }
+// }
+
+//if-else ladder
+
 fn main() {
+    let marks = 85;
+
+    if marks >= 90 {
+        println!("Grade A");
+    } 
+    else if marks >= 75 {
+        println!("Grade B");
+    } 
     
-    let age=20;
+    else if marks >= 50 {
+        println!("Grade C");
+    } 
     
-    if age>18{
-        println!("he/she is an adult");
-    }else{
-        println!("he/she is a child");
+    else {
+        println!("Fail");
     }
 }
