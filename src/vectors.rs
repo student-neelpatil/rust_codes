0dd71de171f78039a4a08a3d957801aa229a514c
student-neelpatil <patilneel288@gmail.com>
2025-11-09 23:51:29 +0530

    rust-vector

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,17 +1,54 @@
+// fn main() {
+// //1st way
+//     // let v=vec![10,20,30,40];//intialization and declaration
 
+//     // v.push(50);
+
+//     // println!("{:?}",v);
+
+// //2nd way
+
+//     let mut v: Vec<u8> = vec![10, 20, 30, 40, 50, 60];
+
+//     v.pop();
+
+//     println!("{:?}", v);
+// }
+
+
+
+//passing vector in function and ownership transfer
 
 fn main(){
-  //1st way
-    // let v=vec![10,20,30,40];//intialization and declaration
+    let v:Vec<u8>=vec![10,20,30,40];
 
-    // v.push(50);
-  
-    // println!("{:?}",v);
+    insert(v);
+
+    // println!("{:?}",v); //here error occur because v is not remain as owner 
+
+}
+
+fn insert(mut vec:Vec<u8>){
+  vec.push(120);
+  println!("{:?}",vec);
+
+} //output-[10, 20, 30, 40, 120]
 
 
-//2nd way
- let v:Vec<u8>=vec![10,20,30,40,50,60];
 
-  println!("{:?}",v);
+//using borrowing
+
+fn main(){
+    let mut v:Vec<u8>=vec![10,20,30,40];
+
+    insert(&mut v);
+
+    println!("{:?}",v);
+
+}
+
+fn insert( vec:&mut Vec<u8>){
+  vec.push(120);
+  println!("{:?}",vec);
 
 }
