ab1f8e7e13e8b500f7675cadd240dfdc3594f5df
student-neelpatil <patilneel288@gmail.com>
2025-11-09 23:58:17 +0530

    rust-structs

--- src/structs.rs
+++ src/structs.rs
@@ -1,78 +1,79 @@
 
-// struct Person{
+struct Person{
   
-//  // name:&str,//In Rust, if a struct contains a reference (like &str),
-//            //you must specify a lifetime — otherwise the compiler 
-//            //doesn’t know how long that reference will be valid.
+ // name:&str,//In Rust, if a struct contains a reference (like &str),
+           //you must specify a lifetime — otherwise the compiler 
+           //doesn’t know how long that reference will be valid.
            
            
-//   name:String,         
-//   age:u8,
+  name:String,         
+  age:u8,
   
   
-// }
+}
 
-// fn main() {
-//     let p1 = Person { name: String::from("neel"), age: 20 };
-//     println!("name is: {}", p1.name);
-//     println!("age is: {}", p1.age);
-// }
+fn main() {
+    let p1 = Person { name: String::from("neel"), age: 20 };
+    println!("name is: {}", p1.name);
+    println!("age is: {}", p1.age);
+}
 
 
 
 //using implementation block
-// struct Person{
+struct Person{
   
-//   name:String,         
-//   age:u8,
+  name:String,         
+  age:u8,
   
   
-// }
+}
 
 
-// impl Person {
-//     // Constructor
-//     fn new(name: String, age: u8) -> Self {
-//         Self { name, age }
-//     }
-// }
+impl Person {
+    // Constructor
+    fn new(name: String, age: u8) -> Self {
+        Self { name, age }
+    }
+}
 
 
-// fn main() {
-//     let p1 = Person::new(String::from("neel"),20);
-//     println!("name is: {}", p1.name);
-//     println!("age is: {}", p1.age);
-// }
+fn main() {
+    let p1 = Person::new(String::from("neel"),20);
+    println!("name is: {}", p1.name);
+    println!("age is: {}", p1.age);
+}
+
 
 //3]using other methods
 
-// struct Person{
+struct Person{
   
-//   name:String,         
-//   age:u8,
+  name:String,         
+  age:u8,
   
   
-// }
+}
 
 
-// impl Person {
-//     // Constructor
-//     fn new(name: String, age: u8) -> Self {
-//         Self { name, age }
-//     }
+impl Person {
+    // Constructor
+    fn new(name: String, age: u8) -> Self {
+        Self { name, age }
+    }
     
     
-//     fn display(&self){
-//       println!("Name: {}, Age: {}", self.name, self.age);
-//     }
-// }
+    fn display(&self){
+      println!("Name: {}, Age: {}", self.name, self.age);
+    }
+}
 
 
-// fn main() {
-//     let p1 = Person::new(String::from("neel"),20);
+fn main() {
+    let p1 = Person::new(String::from("neel"),20);
     
-//     p1.display();
-// }
+    p1.display();
+}
 
 
 //using mutable instance
