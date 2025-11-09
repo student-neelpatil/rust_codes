7114b9709ba865336e2c630d67c1a19d073f83cd
student-neelpatil <patilneel288@gmail.com>
2025-11-09 23:37:28 +0530

    rust-vector

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,14 +1,17 @@
 
 
 fn main(){
+  //1st way
+    // let v=vec![10,20,30,40];//intialization and declaration
 
-    let v=vec![10,20,30,40];//intialization and declaration
-
-    v.push(50);
+    // v.push(50);
   
-    println!("{:?}",v);
+    // println!("{:?}",v);
 
 
+//2nd way
+ let v:Vec<u8>=vec![10,20,30,40,50,60];
 
+  println!("{:?}",v);
 
 }
